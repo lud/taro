f9029d51f36044f80fa64dd1c14accbdedd9788f
niahoo <dev@ooha.in>
2019-05-29 17:55:54 +0200

    refactored action as the first argument inside Action module

--- lib/taro/context/action.ex
+++ lib/taro/context/action.ex
@@ -41,71 +41,64 @@ defmodule Taro.Context.Action do
     match_step(this, step, step_tokens)
   end
 
-  # Action is the subject so it's the first argument. match_tokens()
-  # was implemented with the steps tokens as first argument, beware.
+  def match_step(%__MODULE__{is_regex: true, regex: regex} = this, step, _step_tokens),
+    do: do_match_regex(regex, step.text)
 
-  def match_step(%__MODULE__{is_regex: true, regex: regex} = this, step, _),
-    do: do_match_regex(step.text, regex)
+  def match_step(%__MODULE__{is_regex: false, tokens: tokens} = this, _step, step_tokens),
+    do: match_tokens(tokens, step_tokens)
 
-  def match_step(%__MODULE__{is_regex: false, tokens: tokens} = this, step, step_tokens),
-    do: match_tokens(step_tokens, tokens)
-
-  def match_tokens(step, %Regex{} = re) do
-    do_match_regex(step.text, re)
+  def match_tokens(tokens, step_tokens) do
+    do_match(tokens, step_tokens)
   end
 
-  def match_tokens(tokens, action_tokens) do
-    do_match(tokens, action_tokens)
-  end
-
-  defp do_match(step_tokens, action_tokens, acc \\ [])
+  defp do_match(action_tokens, step_tokens, acc \\ [])
 
   defp do_match([], [], acc),
     do: {:ok, :lists.reverse(acc)}
 
   # Match the same word
-  defp do_match([{:word, word} | step_rest], [{:word, word} | action_rest], acc),
-    do: do_match(step_rest, action_rest, acc)
+  defp do_match([{:word, word} | action_rest], [{:word, word} | step_rest], acc),
+    do: do_match(action_rest, step_rest, acc)
 
   # Match a choice
-  defp do_match([{:word, word} | step_rest], [{:word_choice, {word, _}} | action_rest], acc),
-    do: do_match(step_rest, action_rest, acc)
+  defp do_match([{:word_choice, {word, _}} | action_rest], [{:word, word} | step_rest], acc),
+    do: do_match(action_rest, step_rest, acc)
 
-  defp do_match([{:word, word} | step_rest], [{:word_choice, {_, word}} | action_rest], acc),
-    do: do_match(step_rest, action_rest, acc)
+  defp do_match([{:word_choice, {_, word}} | action_rest], [{:word, word} | step_rest], acc),
+    do: do_match(action_rest, step_rest, acc)
 
-  defp do_match([{:word, word} | step_rest], [{:word_choice, _} | action_rest], acc),
+  defp do_match([{:word_choice, _} | action_rest], [{:word, word} | step_rest], acc),
     do: {:error, :word_choice}
 
   # Match different words
-  defp do_match([{:word, _wordA} | _], [{:word, _wordB} | _], _),
+  defp do_match([{:word, _wordB} | _], [{:word, _wordA} | _], _),
     do: {:error, :word}
 
   # Match a number as string
-  defp do_match([{kind, {_, str}} | step_rest], [{:word, str} | action_rest], acc)
+  defp do_match([{:word, str} | action_rest], [{kind, {_, str}} | step_rest], acc)
        when kind in [:integer, :float],
-       do: do_match(step_rest, action_rest, acc)
+       do: do_match(action_rest, step_rest, acc)
 
-  defp do_match([{kind, {_, _}} | step_rest], [{:word, _} | action_rest], acc)
+  defp do_match([{:word, _} | action_rest], [{kind, {_, _}} | step_rest], acc)
        when kind in [:integer, :float],
        do: {:error, {kind, :word}}
 
   # Accept values
 
-  defp do_match([{kind, {value, _}} | step_rest], [{:accept, _} | action_rest], acc)
+  defp do_match([{:accept, _} | action_rest], [{kind, {value, _}} | step_rest], acc)
        when kind in [:integer, :float],
-       do: do_match(step_rest, action_rest, [value | acc])
+       do: do_match(action_rest, step_rest, [value | acc])
 
-  defp do_match([{:string, value} | step_rest], [{:accept, _} | action_rest], acc),
-    do: do_match(step_rest, action_rest, [value | acc])
+  defp do_match([{:accept, _} | action_rest], [{:string, value} | step_rest], acc),
+    do: do_match(action_rest, step_rest, [value | acc])
 
   # When encountering a single word, it is also capturing the value
-  defp do_match([{:word, word} | step_rest], [{:accept, _} | action_rest], acc),
-    do: do_match(step_rest, action_rest, [word | acc])
+  defp do_match([{:accept, _} | action_rest], [{:word, word} | step_rest], acc),
+    do: do_match(action_rest, step_rest, [word | acc])
 
   # Match a regex. This is a special case, as we must rewrite the input
 
-  defp do_match_regex(step_text, regex) do
+  defp do_match_regex(regex, step_text) do
     case Regex.run(regex, step_text, capture: :all_but_first) do
       nil -> {:error, :regex}
       captures -> {:ok, captures}
